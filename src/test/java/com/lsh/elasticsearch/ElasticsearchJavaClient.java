54cf9bd63a6f8a76a84989afc4adc2e88f8e8d25
LiuShihao <liush99@foxmail.com>
2023-08-08 21:59:47 +0800

    blocking and asynchronous Client

--- src/test/java/com/lsh/elasticsearch/ElasticsearchJavaClient.java
+++ src/test/java/com/lsh/elasticsearch/ElasticsearchJavaClient.java
@@ -1,11 +1,13 @@
 package com.lsh.elasticsearch;
 
 
+import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.TransportUtils;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -19,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import javax.net.ssl.SSLContext;
 import java.io.IOException;
 
+@Slf4j
 public class ElasticsearchJavaClient {
     private static String login = "elastic";
     private static String password = "n6ahTGrZ4JBLTN3ntj9T";
@@ -26,9 +29,11 @@ public class ElasticsearchJavaClient {
     private static int port = 9200;
     private static String fingerprint = "5a1300b9d988ac9840f02a2e074384046c792249d869e36005aa7b00f7d742db";
 
+    private static ElasticsearchTransport transport;
     private static ElasticsearchClient client;
 
     /**
+     * 创建Elasticsearch客户端
      * Connecting Elasticsearch
      * https://www.elastic.co/guide/en/elasticsearch/client/java-api-client/current/connecting.html
      * Create the low-level client
@@ -52,12 +57,42 @@ public class ElasticsearchJavaClient {
                 .build();
 
         // Create the transport and the API client
-        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         client = new ElasticsearchClient(transport);
     }
 
+    /**
+     * 创建阻塞和异步客户端
+     * API clients come in two flavors: blocking and asynchronous. All methods on asynchronous clients return a standard CompletableFuture.
+     */
+    @Test
+    void blockingAndAsynchronousClient() throws IOException {
+        // Synchronous blocking client
+        ElasticsearchClient client = new ElasticsearchClient(transport);
+
+        if (client.exists(b -> b.index("products").id("foo")).value()) {
+            log.info("product exists");
+        }
+
+
+        // Asynchronous non-blocking client
+        ElasticsearchAsyncClient asyncClient = new ElasticsearchAsyncClient(transport);
+        asyncClient
+                .exists(b -> b.index("products").id("foo"))
+                .whenComplete((response, exception) -> {
+                    if (exception != null) {
+                        log.error("Failed to index", exception);
+                    } else {
+                        log.info("Product exists");
+                    }
+                });
+    }
+
+
+
 
     /**
+     * 创建index索引
      * Create an index
      * @throws IOException
      */
